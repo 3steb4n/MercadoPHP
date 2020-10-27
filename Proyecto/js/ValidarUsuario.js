ae54b02f0692d109be31b8efa108d891ea67e01e
Juan Gomez <juangomez1947@outlook.com>
2020-10-27 13:01:00 -0500

    Se agrega archivo .js para la validación de login

---
+++ Proyecto/js/ValidarUsuario.js
@@ -0,0 +1,21 @@
+function verificarLog(userName, userPassword) {
+    var mensaje = document.getElementById("mensaje");
+    alert(userName + "\n" + userPassword);
+
+    if (userName == "" || userPassword == "") {
+        mensaje.innerHTML = "Error. Uno de los campos está vacio. <br> <br>";
+    } else {
+        var XML_start = new XMLHttpRequest();
+        XML_start.onreadystatechange = function () {
+            if(this.status == 200 && this.readyState == 4) {
+                mensaje.innerHTML = this.responseText;
+                if (mensaje.innerHTML == "Error. El usuario no existe o está incorrecto.") {
+                } else {
+                    location.href = '../index.php';
+                }
+            }
+        };
+        XML_start.open('GET', '../Vista/usuarios/ValidarUsuarios.php?usuario='+userName+'&pass='+userPassword, 'true');
+        XML_start.send();
+    }
+}
