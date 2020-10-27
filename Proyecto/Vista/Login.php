b5ad5776a9e0fcdaaf95d9845e339519d8a54a45
Juan Gomez <juangomez1947@outlook.com>
2020-10-27 13:07:10 -0500

    Se realiza instacionc on el archivo ValidarUsuario.js

--- Proyecto/Vista/Login.php
+++ Proyecto/Vista/Login.php
@@ -11,28 +11,27 @@
 	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+	<script src="../js/ValidarUsuario.js"></script>
 
 </head>
 <body>
 	<div class="container">
-	  <div class="abs-center" >
-
-	  		
+	  <div class="abs-center">
 	    <form action="#" class="border p-3 form">
 	    <label for="staticEmail" class="col-sm-2 col-form-label">
 	    	<img src="../Iconos_o_Imagenes/laptop.png" class="mx-auto img-fluid img-thumbnail"  width="150" height="150">
 	    </label>
-
-	    	<h4 class="h4" align="center">Tienda virtual</h1>
+	    <h4 class="h4" align="center">Tienda virtual</h1>
 	      <div class="form-group">
 	        <label for="email">Ingrese Usuario</label>
-	        <input type="email" name="email" id="email" class="form-control">
+	        <input type="text" name="email" id="email" class="form-control">
 	      </div>
 	      <div class="form-group">
 	        <label for="password">Ingrese Contraseña</label>
 	        <input type="password" name="password" id="password" class="form-control">
 	      </div>
-	      <button type="submit" class="btn btn-primary">Ingresar</button>
+		  <span id="mensaje" style="color: red;"></span>
+	      <button type="button" onclick="verificarLog(email.value, password.value)" class="btn btn-primary">Ingresar</button>
 	    </form>
 	  </div>
 	</div>	
