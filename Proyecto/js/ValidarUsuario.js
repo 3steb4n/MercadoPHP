8b74d8734be6ed69ec447b106238d58d392e73cb
Juan Gomez <juangomez1947@outlook.com>
2020-11-10 20:22:10 -0500

    Se cambia condicional del servicio

--- Proyecto/js/ValidarUsuario.js
+++ Proyecto/js/ValidarUsuario.js
@@ -5,16 +5,15 @@ function verificarLog(userName, userPassword) {
         mensaje.innerHTML = "Error. Uno de los campos está vacio. <br> <br>";
     } else {
         var XML_start = new XMLHttpRequest();
-        XML_start.onreadystatechange = function () {
-            if(this.status == 200 && this.readyState == 4) {
+        XML_start.onreadystatechange = function() {
+            if (this.status == 200 && this.readyState == 4) {
                 mensaje.innerHTML = this.responseText;
-                if (mensaje.innerHTML == "Error. El usuario no existe o está incorrecto.") {
-                } else {
+                if (mensaje.innerHTML == "Error. El usuario no existe o está incorrecto.") {} else {
                     location.href = '../index.php';
                 }
             }
         };
-        XML_start.open('GET', '../Vista/usuarios/ValidarUsuarios.php?usuario='+userName+'&pass='+userPassword, 'true');
+        XML_start.open('GET', '../Vista/usuarios/ValidarUsuarios.php?usuario=' + userName + '&pass=' + userPassword, 'true');
         XML_start.send();
     }
 }
