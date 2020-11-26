75c66cb2873f39c4fac599f5edd217401cdd1ff5
Juan Gomez <juangomez1947@outlook.com>
2020-11-26 13:06:14 -0500

    Se agrega validacion de usuarios existentes

--- Proyecto/Vista/usuarios/Registro.php
+++ Proyecto/Vista/usuarios/Registro.php
@@ -1,47 +1,33 @@
 <?php
- 
+
 include '../../PHP/DatabaseConnection.php';
 $connectionInstance = new DatabaseConnection();
-$db = $connectionInstance->ConnectDatabase();
+$connectionDb = $connectionInstance->ConnectDatabase();
 
-  $fechaActual = date("Y-m-d H:i:s");
-  $rol = 2;
-  if (isset($_POST['guardar']))
-    {
-  	  $nombre = $_POST['nombre'];
-      $apellido = $_POST['apellido'];
-      $tipodocumento = $_POST['tipodocumento'];
-      $numerodocumento = $_POST['ndocumento'];
-      $usuario = $_POST['usuario'];
-      $clave = sha1($_POST['clave']);
-      $direccion = $_POST['direccion'];
-      $telefono = $_POST['telefono'];
-      $celular = $_POST['celular'];
-      $email = $_POST['email'];
-      $fechanacimiento = $_POST['fecha'];
-      $ciudad = $_POST['ciudad'];
-    }
+$fechaActual = "2020-09-22 ";
+$rolUser = 2;
+$firstName = $_GET['name'];
+$secondName = $_GET['secondName'];
+$tipDocu = $_GET['tipDocu'];
+$numberDocument = $_GET['numDocument'];
+$emailUser =  $_GET['email'];
+$passwordUser = sha1($_GET['pass']);
+$insertQuery = "INSERT INTO USUARIOS(NOMBRE_USUARIO, APELLIDO_USUARIO, ID_TIP_DOC, DOCUMENTO_USUARIO, USUARIO, CLAVE, EMAIL_USUARIO, FECHA_REGISTRO, ID_ESTADO, ID_ROL) 
+values ('$firstName', '$secondName', $tipDocu, $numberDocument, '$emailUser', '$passwordUser', '$emailUser', '$fechaActual', 1 , 2)";
+$selectQuery = "SELECT * FROM USUARIOS WHERE EMAIL_USUARIO = '$emailUser'";
 
-    echo "$nombre";
-    echo "$apellido";
-    echo "$tipodocumento";
-    echo "$numerodocumento";
-    echo "$usuario";
-    echo "$clave";
-    echo "$direccion";
-    echo "$telefono";
-    echo "$celular";
-    echo "$email";
-    echo "$fechanacimiento";
-    echo "$ciudad";
-    echo "$rol";
-    echo "$fechaActual";
+if($connectionDb){
+  if($queryMysqli = mysqli_query($connectionDb, $selectQuery)){
+      if(mysqli_num_rows($queryMysqli) > 0){
+        echo "Error. Ya hay un usuario registrado con ese email.";
+      }else{
+        if(mysqli_query($connectionDb, $insertQuery)){
+          echo "Usuario registrado correctamente!";
+        }else{
+          echo "Error interno del sistema";
+        }
+      }
+  }
+}
 
-    $query = "INSERT INTO USUARIOS(NOMBRE_USUARIO,APELLIDO_USUARIO,ID_TIP_DOC,DOCUMENTO_USUARIO,USUARIO,CLAVE,DOMICILIO_USUARIO,TELEFONO_USUARIO,CELULAR_USUARIO,EMAIL_USUARIO,FECHA_REGISTRO,FECHA_NACIMIENTO,ID_CIUDAD,ID_ROL,ID_ESTADO,FECHA_MODIFICACION) values ('$nombre','$apellido',$tipodocumento,$numerodocumento,'$usuario','$clave','$direccion',$telefono,$celular,'$email','$fechaActual','$fechanacimiento',$ciudad,$rol,1,'0000-00-00 00:00:00')";
-    // echo "$query";
-    $resultado = mysqli_query($db,$query);
-    if (!$resultado) {
-    	die("consulta fallo");
-    }	
-    header("location:../index.php")
 ?>
