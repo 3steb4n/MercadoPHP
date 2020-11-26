da7ba438a9b28c22f576c614c9ebb974f21a5505
Juan Gomez <juangomez1947@outlook.com>
2020-11-26 13:04:39 -0500

    Se cambio enrutamiento de la BD

--- Proyecto/Vista/productos/ActualizarProductos.php
+++ Proyecto/Vista/productos/ActualizarProductos.php
@@ -1,3 +1,7 @@
+<?php
+error_reporting(0);
+session_start();
+?>
 <!doctype html>
 <html lang="en">
   <head>
@@ -11,7 +15,7 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-    <script src="../js/Pages.js"></script>
+    <script src="../../js/Pages.js"></script>
 	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
@@ -32,15 +36,15 @@
         }
 ?>
     <?php
-include "../PHP/DatabaseConnection.php";
+include "../../PHP/DatabaseConnection.php";
 $connectionInstance = new DatabaseConnection();
 $codProd = "";
 $consultarCategorias = "SELECT NOMBRE_CATEGORIA FROM CATEGORIAS";
 ?>
 	<div class="container">
-		<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-		<a class="navbar-brand" href="#">
-			<img src="../Iconos_o_Imagenes/laptop.png" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark justify-content-between" style="width: 100%;">
+		<a class="navbar-brand" href="../../index.php">
+			<img src="../../Iconos_o_Imagenes/laptop.png" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
 				Tienda Virtual
 			</a>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
@@ -48,37 +52,44 @@ $consultarCategorias = "SELECT NOMBRE_CATEGORIA FROM CATEGORIAS";
 			</button>
 			<div class="collapse navbar-collapse" id="navbarNavDropdown">
 					<ul class="navbar-nav">
+					<?php 
+					if($_SESSION['userName'] == "")
+					{
+					?>
 						<li class="nav-item">
-							<a class="nav-link" href="#">Iniciar Sesion</a>
+							<a class="nav-link" href="Vista/Login.php">Iniciar Sesion</a>
 						</li>
+						<?php
+					}else{
+						?>
+						<li class="nav-item dropdown">
+							<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							<?php echo $_SESSION['userName']; ?>
+							</a>
+							<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+							<?php if($_SESSION['idRol'] == '1') {?>
+                            <a class="dropdown-item" href="../administrador/Usuarios.php">Administrar Usuarios</a>
+                            <?php } ?>
+							<a class="dropdown-item" href="../usuarios/CerrarSesion.php">Cerrar Sesión</a>
+							</div>
+						</li>
+					<?php } ?>
 						<li class="nav-item">
 							<a class="nav-link" href="#">Ofertas</a>
 						</li>
 						<li class="nav-item">
 							<a class="nav-link" href="#">¿Quienes Somos?</a>
 						</li>
-						<li class="nav-item dropdown">
-							<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-								Productos
-							</a>
-							<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-								<a class="dropdown-item" href="#">Licoleria</a>
-								<a class="dropdown-item" href="#">Frutas y Verduras</a>
-								<a class="dropdown-item" href="#">Pasteleria</a>
-								<a class="dropdown-item" href="#">Para el hogar</a>
-								<a class="dropdown-item" href="#">.....</a>
-							</div>
-						</li>
 					</ul>
 				</div>
-				<form class="form-inline my-2 my-lg-0" action="PaginaPrincipal.php" method="get">
+				<form class="form-inline my-2 my-lg-0" action="" method="get">
 					<input class="form-control mr-sm-2" name="nombreProducto" type="search" placeholder="Nombre Producto" aria-label="Search">
 					<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
 				</form>
 				<a class="nav-link" href="#">
-				<img src="../Iconos_o_Imagenes/carro.png" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
+				<img src="../../Iconos_o_Imagenes/carro.png" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
 				</a>
-            </nav>
+			</nav>
             <?php
             $connectionDb = $connectionInstance->ConnectDatabase();
             if ($connectionDb) {
@@ -88,7 +99,7 @@ $consultarCategorias = "SELECT NOMBRE_CATEGORIA FROM CATEGORIAS";
                             ?>
             <div id="formularioActualizar">
                 <div class="form-group">
-                <form action="../PHP/UpdateData.php" method="post" enctype="multipart/form-data">
+                <form action="UpdateData.php" method="post" enctype="multipart/form-data">
                     <label for="exampleInputEmail1">Código del Producto</label>
                     <input type="text" req class="form-control" id="exampleInputEmail1" value="<?php echo $filasDatosProductos[13] ?>" disabled="disabled">
                     <br>
@@ -174,7 +185,7 @@ $consultarCategorias = "SELECT NOMBRE_CATEGORIA FROM CATEGORIAS";
                                     </span>
                                     <input type="text" value="<?php echo $filasDatosProductos[12]; ?>" class="form-control" readonly>
                                 </div>
-                            <img src="../Iconos_o_Imagenes/ProductoImagenes/<?php echo $filasDatosProductos[12]; ?>"  id='img-upload'/>
+                            <img src="../../Iconos_o_Imagenes/ProductoImagenes/<?php echo $filasDatosProductos[12]; ?>"  id='img-upload'/>
                             <input type="hidden" name="codigoProducto" value="<?php echo $filasDatosProductos[13]; $codProd = $filasDatosProductos[13];?>">
                         </div>
                     </div>
@@ -187,12 +198,16 @@ $consultarCategorias = "SELECT NOMBRE_CATEGORIA FROM CATEGORIAS";
             ?>
                 </div>
                 <br>
+                <?php if($_SESSION['idRol'] == 1){ ?>
                 <button type="submit" class="btn btn-info">Actualizar Producto</button>
+                <?php } ?>
                 </form>
                 <br>
-                <form action="../PHP/DeleteData.php" method="post">
+                <?php if($_SESSION['idRol'] == 1){ ?>
+                <form action="DeleteData.php" method="post">
                 <input type="hidden" name="codigoProducto" value="<?php echo $codProd;?>">
                 <button type="submit" name="eliminarProducto" class="btn btn-danger">Eliminar Producto</button>
+                <?php } ?>
                 </form>
             </div>
         </div>
